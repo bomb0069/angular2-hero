7c125706e4e228a91ee192bb031c001d49c3357b
Karan Sivarat <bomb0069@gmail.com>
2017-03-11 13:32:42 +0700

    implement class Hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>',
+  template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>',
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = {id: 1, name: 'Windstorm'};
+}
+
+class Hero {
+  id: number;
+  name: String;
 }
