2e99de13afc4956d199769c79278bdac6251fa5f
Karan Sivarat <bomb0069@gmail.com>
2017-03-11 13:24:30 +0700

    add hero detail

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,11 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
   }));
+
+  it('should render hero details in a h2 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('Windstorm details!');
+  }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<h1>{{title}}</h1>',
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>',
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
